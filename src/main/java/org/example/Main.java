a78cbc7140d698305d844e52b75b2ce1a48644e7
Dimix88 <dimitriruzandra1@gmail.com>
2025-02-12 18:52:30 +0200

    Second Commit, Adding Operations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,14 +16,58 @@ public class Main {
             System.out.println("5.  Exit");
             System.out.println("Choose an option: ");
 
+            if(scanner.hasNextInt()){
+                System.out.println("Invalid Input");
+                scanner.next();
+                continue;
 
+            }
             int choice = scanner.nextInt();
 
             if(choice == 5){
                 System.out.println("Closing Program");
                 break;
             }
+            //User input
+            System.out.println("Enter First Number: ");
+            double num1 = scanner.nextInt();
+            System.out.println("Enter Second Number: ");
+            double num2 = scanner.nextInt();
+
+            double result = 0;
+            boolean valid = true;
+
+            switch(choice){
+                case 1:
+                    result = num1 + num2;
+                    break;
+
+                    case 2:
+                        result = num1 - num2;
+                        break;
+
+                        case 3:
+                            result = num1 * num2;
+                            break;
+                            case 4:
+                                if(num2 != 0){
+                                    result = num1 / num2;
+                                }
+                                else{
+                                    System.out.println("Cannot divide by zero");
+                                    valid = false;
+                                }
+                                break;
+                                default:
+                                    System.out.println("Invalid Option, Try Again");
+                                    valid = false;
+
+            }
+            if(valid){
+                System.out.println("Result: " + result);
+            }
 
         }
+        scanner.close();
     }
 }
